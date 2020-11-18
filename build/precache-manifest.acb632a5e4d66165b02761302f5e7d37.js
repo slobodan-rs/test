d3843a7ee39ae3d1cc9584daa2c89d14cbd8a48a
Slobodan <mr.slobodan@hotmail.rs>
2020-11-18 14:01:39 +0100

    test

---
+++ build/precache-manifest.acb632a5e4d66165b02761302f5e7d37.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe2fec255cd8c890fc77f77c3b582c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39b0e35894ca9bfc6ed5",
+    "url": "/static/css/main.af9e22cb.chunk.css"
+  },
+  {
+    "revision": "243b445e78015a085a6a",
+    "url": "/static/js/2.22c5d253.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.22c5d253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39b0e35894ca9bfc6ed5",
+    "url": "/static/js/main.d4a0b33d.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  },
+  {
+    "revision": "7616bd43b30b9c8bfd709209a2e59d23",
+    "url": "/static/media/12.7616bd43.png"
+  },
+  {
+    "revision": "f82e59f5dfee9c5277175f8b356f9901",
+    "url": "/static/media/13.f82e59f5.png"
+  },
+  {
+    "revision": "c6e990408848fc4799bd86c1255e4026",
+    "url": "/static/media/14.c6e99040.png"
+  },
+  {
+    "revision": "b1ba7dc64f9f56783c6f911a3df5e2bb",
+    "url": "/static/media/15.b1ba7dc6.png"
+  },
+  {
+    "revision": "04163a0d82e967fc99edbbb1411ef352",
+    "url": "/static/media/16.04163a0d.png"
+  },
+  {
+    "revision": "f70faf72046b1ed2ed213b998ac2556b",
+    "url": "/static/media/17.f70faf72.png"
+  },
+  {
+    "revision": "d88efc61fcbdc9ff2b8a3376aa580724",
+    "url": "/static/media/18.d88efc61.png"
+  },
+  {
+    "revision": "dd331f513ca0a273794785760a73b496",
+    "url": "/static/media/6.dd331f51.png"
+  },
+  {
+    "revision": "7943459c012a5f3fab4975579472d5ad",
+    "url": "/static/media/7.7943459c.png"
+  },
+  {
+    "revision": "565c3dcd0410c9fa34c77f77b6e8461e",
+    "url": "/static/media/8.565c3dcd.png"
+  },
+  {
+    "revision": "bf44d7c0f24c2ac3d200d8a28edacd8d",
+    "url": "/static/media/Xnomad.bf44d7c0.svg"
+  },
+  {
+    "revision": "06eb2fd372b980fdf0439fbcad12aa5d",
+    "url": "/static/media/XnomadMobile.06eb2fd3.svg"
+  },
+  {
+    "revision": "1796d27427dc78c65020fe09d8ae9820",
+    "url": "/static/media/airbnb-mobile.1796d274.svg"
+  },
+  {
+    "revision": "bb7fc307e165b5fed10373941738703a",
+    "url": "/static/media/airbnb.bb7fc307.svg"
+  },
+  {
+    "revision": "819d28330b79f89d6497218cda4ba4ea",
+    "url": "/static/media/arrow.819d2833.svg"
+  },
+  {
+    "revision": "130a5245d5cc8a6da6318f16b55111ea",
+    "url": "/static/media/book-mobile.130a5245.svg"
+  },
+  {
+    "revision": "6e5d3828973ee409e4e82be02d3475d6",
+    "url": "/static/media/book.6e5d3828.svg"
+  },
+  {
+    "revision": "76f78a677f2c51bb2bef4fd1b8890224",
+    "url": "/static/media/burger.76f78a67.svg"
+  },
+  {
+    "revision": "177f94578b9d1ec398661fce931eff54",
+    "url": "/static/media/calendar.177f9457.svg"
+  },
+  {
+    "revision": "87392799898ecffebd2414780721d1da",
+    "url": "/static/media/chart1.87392799.svg"
+  },
+  {
+    "revision": "96d79510864e7f62edbd6f794ae8bd45",
+    "url": "/static/media/chart2.96d79510.svg"
+  },
+  {
+    "revision": "7cc41108e7cb3e6efd161d2ea1c12e71",
+    "url": "/static/media/chart3.7cc41108.svg"
+  },
+  {
+    "revision": "50f7f09cb21a0f5f2195c042e3354e93",
+    "url": "/static/media/featured-spaces-4.50f7f09c.jpg"
+  },
+  {
+    "revision": "fd51027a5cef18443d684a17fc6f358f",
+    "url": "/static/media/image.fd51027a.png"
+  },
+  {
+    "revision": "71a0b9888a48f844dcb60ef23c7c74c4",
+    "url": "/static/media/instagram.71a0b988.svg"
+  },
+  {
+    "revision": "084dc2b8cff543485f11aac6845ca7f4",
+    "url": "/static/media/landing-img1.084dc2b8.jpg"
+  },
+  {
+    "revision": "b364ac7fb99ffffa33047ef23bf61e0e",
+    "url": "/static/media/landing-img2.b364ac7f.jpeg"
+  },
+  {
+    "revision": "d84333f8d3011eb689db5cb07afe400a",
+    "url": "/static/media/landing-img3.d84333f8.jpeg"
+  },
+  {
+    "revision": "c445659a87542dc99f86447158908d93",
+    "url": "/static/media/minus-staffing.c445659a.svg"
+  },
+  {
+    "revision": "24489c314cef72c0a6c934f6c12ae36f",
+    "url": "/static/media/partners-mobile.24489c31.svg"
+  },
+  {
+    "revision": "1234ede383a048b953f9022aeec09ad2",
+    "url": "/static/media/partners.1234ede3.svg"
+  },
+  {
+    "revision": "e03e089d4111a87f4b0b0153e022c3df",
+    "url": "/static/media/perfect-space.e03e089d.jpeg"
+  },
+  {
+    "revision": "699413667f46a4143b33f90ad12e8bd7",
+    "url": "/static/media/plus-staffing.69941366.svg"
+  },
+  {
+    "revision": "a24a1b91fdaf97d38c43c165605ce3d3",
+    "url": "/static/media/search.a24a1b91.svg"
+  },
+  {
+    "revision": "362de831226cb09c051916b4f4894427",
+    "url": "/static/media/success-arrow.362de831.svg"
+  },
+  {
+    "revision": "33349abaec5555fb5beed07ddf6dbcc8",
+    "url": "/static/media/success-location.33349aba.svg"
+  },
+  {
+    "revision": "e6a6b2be0558319f5b81594c5ca7db0d",
+    "url": "/static/media/trusted-mobile.e6a6b2be.svg"
+  },
+  {
+    "revision": "c9340c19772d765c13929f06a067c393",
+    "url": "/static/media/tryck-16.c9340c19.jpg"
+  },
+  {
+    "revision": "a2dc38816ffe742f4036f4dfe9d9e94b",
+    "url": "/static/media/twitter.a2dc3881.svg"
+  }
+]);
